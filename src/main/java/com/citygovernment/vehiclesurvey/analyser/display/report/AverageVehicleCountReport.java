95c4edd47794ead3edad1146e245e9c075c6169b
lordlion <lordlion@FelineFury>
2017-04-23 18:02:51 +0530

    Coding of AverageVehicleCountReport done.

--- src/main/java/com/citygovernment/vehiclesurvey/analyser/display/report/AverageVehicleCountReport.java
+++ src/main/java/com/citygovernment/vehiclesurvey/analyser/display/report/AverageVehicleCountReport.java
@@ -1,9 +1,19 @@
 package com.citygovernment.vehiclesurvey.analyser.display.report;
 
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
 
+import com.citygovernment.vehiclesurvey.analyser.Application;
 import com.citygovernment.vehiclesurvey.analyser.analysis.Analysis;
+import com.citygovernment.vehiclesurvey.analyser.analysis.DailyAnalysis;
+import com.citygovernment.vehiclesurvey.analyser.analysis.Direction;
 import com.citygovernment.vehiclesurvey.analyser.analysis.Vehicle;
 
 /**
@@ -28,37 +38,70 @@ public class AverageVehicleCountReport extends AReport{
 	
 	
 	/* (non-Javadoc)
+	 * @see com.citygovernment.vehiclesurvey.analyser.display.report.AReport#show(com.citygovernment.vehiclesurvey.analyser.analysis.Analysis)
+	 */
+	@Override
+	public void show(Analysis analysis) {
+		
+		this.analysis = analysis;
+		try (OutputStream os = new FileOutputStream(outputFilePath.toFile()); PrintStream printStream = new PrintStream(os);) {
+			Application.LOGGER.info("Generate report file - " + outputFilePath);
+			this.printStream = printStream;
+			printStream.print("\t*** Average Vehicle Count Report ***\n");
+			ArrayList<Vehicle> vehicleList = new ArrayList<>();
+			for (Iterator<DailyAnalysis> iterator = analysis.getDailyAnalysisList().iterator(); iterator.hasNext();) {
+				DailyAnalysis dayWiseAnalysis = iterator.next();
+				
+				vehicleList.addAll(dayWiseAnalysis.getVehiclesPassed());
+			}
+			Integer AverageVehicleCount = vehicleList.size();
+			printStream.print("\n\t\tAverage vehicle count across all the days = " + AverageVehicleCount/this.analysis.getDailyAnalysisList().size());
+			
+			printStream.print("\n\t\t\tMorning");
+			computeForMorning(vehicleList,  b -> e -> v -> calculationForEachIntervalInPeriodMethod( b, e, v));
+			printStream.print("\n\t\t\tEvening");
+			computeForEvening(vehicleList,  b -> e -> v -> calculationForEachIntervalInPeriodMethod( b, e, v));
+			
+			computeForAllOtherPeriods(vehicleList, s -> preCalculationForAPeriodMethod(s),  b -> e -> v -> calculationForEachIntervalInPeriodMethod( b, e, v), this::postCalculationForAPeriodMethod);
+			
+		} catch (Exception e2) {
+			Application.LOGGER.log(Level.SEVERE, "Could not show Report.", e2);
+		}
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see com.citygovernment.vehiclesurvey.analyser.display.report.AReport#preCalculationForAPeriodMethod(java.lang.String)
+	 */
+	@Override
+	protected void preCalculationForAPeriodMethod(String strPeriodName) {
+		printStream.print("\n\t\t\t"+strPeriodName);
+	}
+
+	/* (non-Javadoc)
 	 * @see com.citygovernment.vehiclesurvey.analyser.display.report.AReport#calculationForEachIntervalInPeriodMethod(java.time.LocalTime, java.time.LocalTime, java.util.List)
 	 */
 	@Override
 	protected void calculationForEachIntervalInPeriodMethod(LocalTime intervalStart, LocalTime intervalStop, List<Vehicle> vehiclesList) {
+		List<Vehicle> vehiclesDuringPeriod = vehiclesList.stream().filter(v -> v.getPassingTime().isAfter(intervalStart) && v.getPassingTime().isBefore(intervalStop)).collect(Collectors.toList());
+		printStream.print("\n\t\t\t\tAverage vehicle count (" + Application.DATE_TIME_FORMATTER.format(intervalStart) + " to " + Application.DATE_TIME_FORMATTER.format(intervalStop) + ") = "
+				+ findAverageCount(vehiclesDuringPeriod,this.analysis.getDailyAnalysisList().size()));
+		
+		List<Vehicle> vehiclesNorthbound = vehiclesDuringPeriod.stream().filter(v -> v.getDirection().equals(Direction.NORTH)).collect(Collectors.toList());
+		printStream.print("\n\t\t\t\t\tNorthbound average vehicle count (" + Application.DATE_TIME_FORMATTER.format(intervalStart) + " to " + Application.DATE_TIME_FORMATTER.format(intervalStop) + ") = "
+				+ findAverageCount(vehiclesNorthbound,this.analysis.getDailyAnalysisList().size()));
+		List<Vehicle> vehiclesSouthbound = vehiclesDuringPeriod.stream().filter(v -> v.getDirection().equals(Direction.SOUTH)).collect(Collectors.toList());
+		printStream.print("\n\t\t\t\t\tSouthbound average vehicle count (" + Application.DATE_TIME_FORMATTER.format(intervalStart) + " to " + Application.DATE_TIME_FORMATTER.format(intervalStop) + ") = "
+				+ findAverageCount(vehiclesSouthbound,this.analysis.getDailyAnalysisList().size()));
+	}
 
-		}
-	
-	
 	/**
 	 * Finds average number of vehicles in the provided list.
 	 * @param vehiclesList List of vehicles.
 	 * @return Count of vehicles.
 	 */
 	private long findAverageCount(List<Vehicle> vehiclesList, int numberOfDays) {
-				//TODO
-		return 0;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.citygovernment.vehiclesurvey.analyser.display.report.AReport#preCalculationForAPeriodMethod(java.lang.String)
-	 */
-	@Override
-	protected void preCalculationForAPeriodMethod(String strPeriodName) {
-		// TODO
-	}
-
-	/* (non-Javadoc)
-	 * @see com.citygovernment.vehiclesurvey.analyser.display.report.AReport#show(com.citygovernment.vehiclesurvey.analyser.analysis.Analysis)
-	 */
-	@Override
-	public void show(Analysis analysis) {
-		// TODO
+		long averageCount = vehiclesList.size()/numberOfDays;
+		return averageCount;
 	}
 }
